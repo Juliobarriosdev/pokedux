8141e654fc8bef2e3d3730bbf5cd423f63292602
Julio Barrios <julio.barrios.9618@gmail.com>
2022-07-25 18:15:36 -0300

    Middlewares

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 import { pokemonsReducer } from './reducers/pokemons';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import { compose, applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { logger, featuring } from './middlewares';
 
 import './index.css';
 import 'antd/dist/antd.css';
 import App from './App';
 
-const store = createStore(pokemonsReducer)
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
+);
+
+const store = createStore(
+  pokemonsReducer,
+  composedEnhancers
+  )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/middlewares/index.js
@@ -0,0 +1,13 @@
+export const logger = (store) => (next) => (action) => {
+  console.log(action);
+  next(action); //Envia el action al reducer
+};
+
+export const featuring = (store) => (next) => (actionInfo) => {
+  const featured = [{ name: 'eddie'}, ...actionInfo.action.payload];
+  const updatedActionInfo = {
+    ...actionInfo,
+    action: { ...actionInfo.action, payload: featured }
+  };
+  next(updatedActionInfo);
+}
